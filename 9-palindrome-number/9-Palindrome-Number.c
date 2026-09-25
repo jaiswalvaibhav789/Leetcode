c226430549107da6454eb0c6ebb13c384e3294c2
Vaibhav Jaiswal <143876521+jaiswalvaibhav789@users.noreply.github.com>
2026-09-26 00:34:55 +0530

    [9] [Time Beats: 100.00%] [Memory Beats: 5.82%] - LeetPush

---
+++ 9-palindrome-number/9-Palindrome-Number.c
@@ -0,0 +1,16 @@
+#include <stdbool.h>
+
+bool isPalindrome(int x) {
+    int original = x;
+    long long rev = 0;
+
+    if (x < 0)
+        return false;
+
+    while (x != 0) {
+        rev = rev * 10 + x % 10;
+        x = x / 10;
+    }
+
+    return original == rev;
+}
